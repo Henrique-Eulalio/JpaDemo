e3ee5d17640503e2ea1a687ed999673d5a91b848
Henrique-Eulalio <henrique.eulalio@etec.sp.gov.br>
2021-02-25 19:25:17 -0300

    First Commit

---
+++ src/main/java/com/projeto/jpa/javajpademo/JavajpademoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.jpa.javajpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavajpademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavajpademoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/jpa/javajpademo/JavajpademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.jpa.javajpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavajpademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
