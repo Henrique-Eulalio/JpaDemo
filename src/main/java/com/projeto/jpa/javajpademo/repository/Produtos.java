ac84dc20366d849eab835784d0a30ebf972affcb
perucello <perucello@hotmail.com>
2021-02-25 22:14:30 -0300

    criado JpaRepository - classe extendida de Produto

--- src/main/java/com/projeto/jpa/javajpademo/repository/Produtos.java
+++ src/main/java/com/projeto/jpa/javajpademo/repository/Produtos.java
@@ -1,6 +1,11 @@
 package com.projeto.jpa.javajpademo.repository;
 
-public class Produtos {
+import org.springframework.data.jpa.repository.JpaRepository;
 
-} 
+import com.projeto.jpa.javajpademo.Model.Produto;
+
+
+public interface  Produtos  extends JpaRepository<Produto, Long> {
+
+}
  
